719bababafda8792d41d3de3bb3cad6af7a2555f
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-05-14 20:24:16 +0200

    initial ci

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import AppBar from 'material-ui/AppBar';
+
+const Navbar = () =>
+    <AppBar title="Images Finder"/>
+
+export default Navbar
---
+++ src/components/results/Results.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Results extends Component {
+
+  render() {
+    return (
+      <div>
+        Results
+      </div>
+    );
+  }
+}
+
+
+export default Results;
