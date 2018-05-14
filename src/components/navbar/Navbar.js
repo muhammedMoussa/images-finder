a4366dbbd00e34e70721c100356f410c552952cf
muhammedMoussa <muhammedmoussa@hotmail.com>
2018-05-14 20:32:56 +0200

    edit appbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,6 @@ import React from 'react'
 import AppBar from 'material-ui/AppBar';
 
 const Navbar = () =>
-    <AppBar title="Images Finder"/>
+    <AppBar title="Images Finder" style={{background: '#f44336', textAlign: 'center'}}/>
 
 export default Navbar
